00a64222d9db82e636c4d505c6ad1a3fa02a80ab
Zhandos200 <dildahanz@mail.ru>
2025-02-15 23:17:22 +0500

    hello sayan

--- saian.go
+++ saian.go
@@ -8,9 +8,3 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
-
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
